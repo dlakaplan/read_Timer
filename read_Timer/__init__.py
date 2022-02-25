29d2cf8b1907f36c5e7201ec4d54440a783b253d
David Kaplan <kaplan@uwm.edu>
2022-02-25 13:58:14 -0600

    now reads subints

--- read_Timer/__init__.py
+++ read_Timer/__init__.py
@@ -81,7 +81,10 @@ def get_definition(filename):
         else:
             continue
         if vartype == "char":
-            length = chararray_lengths[varname.split("[")[1].replace("]", "")]
+            try:
+                length = int(varname.split("[")[1].replace("]", ""))
+            except ValueError:
+                length = chararray_lengths[varname.split("[")[1].replace("]", "")]
             varname = varname.split("[")[0]
         keywords[varname] = Keyword(name=varname, dtype=vartype)
         if vartype == "char":
@@ -102,7 +105,8 @@ class Keyword:
             try:
                 result = out.decode().rstrip("\x00")
             except UnicodeDecodeError:
-                logger.warning(f"Unable to decode contents of '{varname}'")
+                logger.warning(f"Unable to decode contents of '{self.name}'")
+                result = None
         elif self.dtype == "int":
             result = struct.unpack(_endian + "i", out)[0]
         elif self.dtype == "float":
@@ -146,6 +150,9 @@ class Band:
         results = struct.unpack(">" + self.sequence, out)
         for k, r in zip(self.keywords, results):
             self.keywords[k].value = r
+        self.frequency = self["centrefreq"].value * u.MHz
+        self.bandwidth = self["bw"].value * u.MHz
+        self.npol = self["npol"].value
 
     def asstr(self):
         s = []
@@ -154,7 +161,47 @@ class Band:
         return s
 
     def __repr__(self):
-        s = f"{self['bw'].value} MHz band at {self['centrefreq'].value} MHz"
+        s = f"{self.bandwidth} Band at {self.frequency}"
+        return s
+
+    def __str__(self):
+        return repr(self) + ":\n" + ("\n".join(self.asstr()))
+
+    def __getitem__(self, key):
+        return self.keywords[key]
+
+    def __setitem__(self, key, value):
+        self.keywords[key].value = value
+
+
+class Subint:
+    def __init__(self, number=None):
+        self.keywords = copy.deepcopy(_subint_keyword_definition)
+        self.number = number
+
+    @property
+    def size(self):
+        size = 0
+        for k in self.keywords:
+            size += self.keywords[k].size
+        return size
+
+    def read(self, fptr):
+        for varname in self.keywords:
+            self.keywords[varname].read(fptr)
+        self.starttime = Time(
+            self.keywords["mjd"].value + self.keywords["fracmjd"].value, format="mjd"
+        )
+        self.integration = self.keywords["integration"].value * u.s
+
+    def asstr(self):
+        s = []
+        for key in self.keywords:
+            s.append(str(self.keywords[key]))
+        return s
+
+    def __repr__(self):
+        s = f"Subint {self.number} at {self.starttime} for {self.integration}"
         return s
 
     def __str__(self):
@@ -183,11 +230,17 @@ class TimerHeader:
     duration
     position
     psrname
+    polyco
+    ephem
     """
 
-    def __init__(self, filename):
+    def __init__(self, filename=None):
         self.keywords = copy.deepcopy(_timer_keyword_definition)
+        self.subints = []
+        if filename is not None:
+            self.read(filename)
 
+    def read(self, filename):
         logger.debug(f"Reading Timer file {filename}")
         self.filename = filename
         f = open(filename, "rb")
@@ -196,13 +249,24 @@ class TimerHeader:
                 result = Band(name=varname)
                 result.read(f)
                 self.keywords[varname].value = result
+                self.keywords[varname].size = result.size
             else:
                 self.keywords[varname].read(f)
 
+        # ignore the backend info
+        out = f.read(self.keywords["be_data_size"].value)
+
+        if self.keywords["nbytespoly"].value > 0:
+            out = f.read(self.keywords["nbytespoly"].value)
+            self.polyco = out.decode().rstrip("\x00")
+
+        out = f.read(self.keywords["nbytesephem"].value)
+        self.ephem = out.decode().rstrip("\x00")
+
         # process some of the more useful keywords
-        self.starttime = Time(
-            self.keywords["mjd"].value + self.keywords["fracmjd"].value, format="mjd"
-        )
+        # self.starttime = Time(
+        #    self.keywords["mjd"].value + self.keywords["fracmjd"].value, format="mjd"
+        # )
         if self.keywords["coord_type"].value == "05":
             self.position = SkyCoord(
                 self.keywords["ra"].value * u.rad, self.keywords["dec"].value * u.rad
@@ -220,15 +284,60 @@ class TimerHeader:
             self.position = None
 
         self.telescope = self.keywords["telid"].value
-        self.duration = (
-            self.keywords["nsub_int"].value * self.keywords["sub_int_time"].value * u.s
-        )
-        self.stoptime = self.starttime + self.duration
         self.psrname = self.keywords["psrname"].value
+        self.nchannels = self.keywords["nsub_band"].value
+        # is this right
+        self.npol = self.keywords["banda"].value["npol"].value
+        logger.debug(f"Reading {self.keywords['nsub_int'].value} subints")
+        self.duration = 0 * u.s
+        self.read_subints(f)
+        # set the start time to the start of the first subint
+        self.starttime = self.subints[0].starttime
         logger.debug(f"Telescope = {self.telescope}")
         logger.debug(f"Pulsar = {self.psrname}")
         logger.debug(f"Start = {self.starttime.mjd} = {self.starttime.iso}")
         logger.debug(f"Duration = {self.duration}")
+
+        # this is just for the first subint
+        # self.duration = (
+        #    self.keywords["nsub_int"].value * self.keywords["sub_int_time"].value * u.s
+        # )
+        self.stoptime = self.starttime + self.duration
+
+    def read_subints(self, fptr):
+        for subint in range(self.keywords["nsub_int"].value):
+            self.subints.append(Subint(number=subint))
+            self.subints[-1].read(fptr)
+            self.duration += self.subints[-1].integration
+            fptr.seek(self.subint_data_size, 1)
+
+    @property
+    def size(self):
+        size = 0
+        for k in self.keywords:
+            size += self.keywords[k].size
+        return size
+
+    @property
+    def subint_data_size(self):
+        if self["wts_and_bpass"]:
+            # stores data + weights + bandpass
+            size = self.nchannels * _data_lengths["float"] * (1 + 2 * self.npol)
+        else:
+            # looks like float(scale) + float(offset) + nbin*nchan*npol*2
+            # CHECK
+            size = (
+                _data_lengths["float"] * 2
+                + self.nchannels * self.npol * self["nbin"].value * 2
+            )
+        # floats for centrefreq, wt
+        # ints for nbin, poln
+        size += self.nchannels * (_data_lengths["float"] * 2 + _data_lengths["int"] * 2)
+        # has 2 floats + nbin * 2byte integers
+        # CHECK: this is in the "no_amps" case
+        # otherwise it's more complicated?
+        size += self.nchannels * (2 * _data_lengths["float"] + self["nbin"].value * 2)
+        return size
 
     def asstr(self):
         s = []
@@ -252,3 +361,4 @@ class TimerHeader:
 
 _timer_keyword_definition = get_definition("timer.h")
 _band_keyword_definition = get_definition("band.h")
+_subint_keyword_definition = get_definition("mini.h")
